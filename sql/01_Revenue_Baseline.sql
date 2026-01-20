cfc003fa7e289c3e369eb897199594e0b5fe332a
srai19 <88417823+srai19@users.noreply.github.com>
2026-01-20 22:41:40 +0530

    Update 01_Revenue_Baseline.sql

--- sql/01_Revenue_Baseline.sql
+++ sql/01_Revenue_Baseline.sql
@@ -1,1 +1,10 @@
+-- Business Question:
+-- What is the overall revenue and scale of the e-commerce platform?
+
+SELECT
+    COUNT(DISTINCT Order_ID) AS total_orders,
+    COUNT(DISTINCT Customer_ID) AS total_customers,
+    SUM(Total_Amount) AS total_revenue,
+    AVG(Total_Amount) AS avg_order_value
+FROM ecommerce_orders;
 
