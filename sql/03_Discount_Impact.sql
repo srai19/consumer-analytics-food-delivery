b8afd2df686f26063afcd916ddc67deaa4a2f9ab
srai19 <88417823+srai19@users.noreply.github.com>
2026-01-22 00:30:27 +0530

    Update 03_Discount_Impact.sql

--- sql/03_Discount_Impact.sql
+++ sql/03_Discount_Impact.sql
@@ -1,1 +1,14 @@
+-- Business Question:
+-- How do discounts impact revenue and order value?
+
+SELECT
+    CASE 
+        WHEN Discount_Amount > 0 THEN 'Discounted Orders'
+        ELSE 'Non-Discounted Orders'
+    END AS discount_flag,
+    COUNT(DISTINCT Order_ID) AS total_orders,
+    SUM(Total_Amount) AS total_revenue,
+    AVG(Total_Amount) AS avg_order_value
+FROM ecommerce_orders
+GROUP BY discount_flag;
 
