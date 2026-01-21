b42fd63631f69aa43eb96da4119611a996dc3b67
srai19 <88417823+srai19@users.noreply.github.com>
2026-01-21 23:46:26 +0530

    Update 02_Retention_Profitability.sql

--- sql/02_Retention_Profitability.sql
+++ sql/02_Retention_Profitability.sql
@@ -1,1 +1,13 @@
+-- Business Question:
+-- Are returning customers more valuable and profitable than new customers?
+
+SELECT
+    Is_Returning_Customer,
+    COUNT(DISTINCT Customer_ID) AS customers,
+    COUNT(DISTINCT Order_ID) AS total_orders,
+    SUM(Total_Amount) AS total_revenue,
+    AVG(Total_Amount) AS avg_order_value,
+    SUM(Discount_Amount) AS total_discount_given
+FROM ecommerce_orders
+GROUP BY Is_Returning_Customer;
 
