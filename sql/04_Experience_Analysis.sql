12aef824f1c2bd775854ef6a38345c6f16bcc15a
srai19 <88417823+srai19@users.noreply.github.com>
2026-01-25 17:47:10 +0530

    Update 04_Experience_Analysis.sql

--- sql/04_Experience_Analysis.sql
+++ sql/04_Experience_Analysis.sql
@@ -1,1 +1,11 @@
+-- Business Question:
+-- How does delivery experience impact customer satisfaction?
+
+SELECT
+    Delivery_Time_Days,
+    AVG(Customer_Rating) AS avg_rating,
+    COUNT(DISTINCT Order_ID) AS total_orders
+FROM ecommerce_orders
+GROUP BY Delivery_Time_Days
+ORDER BY Delivery_Time_Days;
 
